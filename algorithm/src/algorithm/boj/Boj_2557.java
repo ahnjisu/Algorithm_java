5f5b4eeb8f6d3bf8919523c26ccfeeb11d8ee529
ahnjisu <an1302@hs.ac.kr>
2022-12-19 15:29:30 +0900

    Boj 2557 Hello World!

---
+++ algorithm/src/algorithm/boj/Boj_2557.java
@@ -0,0 +1,12 @@
+package algorithm.boj;
+
+// https://www.acmicpc.net/problem/2557
+
+public class Boj_2557 {
+	public static void main(String[] ar){
+		System.out.print("Hello World!");
+	}
+	
+}
+
+//	memory : 13928KB, time : 116ms
