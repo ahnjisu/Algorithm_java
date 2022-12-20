b5e73ef460d5467aabd3d4f40bad5bac78ef74be
ahnjisu <an1302@hs.ac.kr>
2022-12-20 17:27:07 +0900

    Boj 10926 ??!

---
+++ algorithm/src/algorithm/boj/Boj_10926.java
@@ -0,0 +1,16 @@
+package algorithm.boj;
+import java.util.*;
+// https://www.acmicpc.net/problem/10926
+
+public class Boj_10926 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		String id = sc.next();
+		System.out.print(id+"??!");
+	}
+
+}
+// memory : 17720KB, time : 204ms
