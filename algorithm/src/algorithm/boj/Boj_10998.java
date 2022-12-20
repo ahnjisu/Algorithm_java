26a8b7ebc2f173acabea704ce32ac5bfc4dfeaf7
ahnjisu <an1302@hs.ac.kr>
2022-12-20 17:26:10 +0900

    Boj 10998 a * b

---
+++ algorithm/src/algorithm/boj/Boj_10998.java
@@ -0,0 +1,16 @@
+package algorithm.boj;
+import java.util.*;
+// https://www.acmicpc.net/problem/10998
+	
+public class Boj_10998 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.print(a*b);
+	}
+
+}
+// memory : 17692KB, time : 208ms
