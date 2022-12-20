c8838d2da92944f0c93ea01f2fbd94e97aa4581d
ahnjisu <an1302@hs.ac.kr>
2022-12-20 17:20:37 +0900

    Boj 1000 a + b

---
+++ algorithm/src/algorithm/boj/Boj_1000.java
@@ -0,0 +1,15 @@
+package algorithm.boj;
+import java.util.*;
+// https://www.acmicpc.net/problem/1000
+
+public class Boj_1000 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a + b);
+	}
+}
+
+//memory : 17728KB, time : 212ms
