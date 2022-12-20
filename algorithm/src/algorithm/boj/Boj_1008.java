8f9a3f59fe82cf5b49ac643d9ebeefee448360d1
ahnjisu <an1302@hs.ac.kr>
2022-12-20 17:26:23 +0900

    Boj 1008 a / b

---
+++ algorithm/src/algorithm/boj/Boj_1008.java
@@ -0,0 +1,19 @@
+package algorithm.boj;
+import java.util.*;
+// https://www.acmicpc.net/problem/1008
+
+public class Boj_1008 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.print((double)a/b);
+	}
+
+}
+
+// memory : 17836KB, time : 208ms
