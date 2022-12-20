000e296dad3d091b18d8c9f3fd331a881ddf1f28
ahnjisu <an1302@hs.ac.kr>
2022-12-20 17:20:55 +0900

    Boj 1001 a - b

---
+++ algorithm/src/algorithm/boj/Boj_1001.java
@@ -0,0 +1,14 @@
+package algorithm.boj;
+import java.util.*;
+// https://www.acmicpc.net/problem/1001
+
+public class Boj_1001 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		System.out.print(a-b);
+	}
+}
+
+//memory : 17756KB, time : 208ms
