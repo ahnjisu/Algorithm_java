5073097dfa34baf2a7f0311908d4cfac415673de
ahnjisu <an1302@hs.ac.kr>
2022-12-20 17:26:45 +0900

    Boj 10869 사칙연산

---
+++ algorithm/src/algorithm/boj/Boj_10869.java
@@ -0,0 +1,23 @@
+package algorithm.boj;
+import java.util.*;
+//https://www.acmicpc.net/problem/10869
+
+public class Boj_10869 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+
+}
+
+// memory : 17724KB, time : 212ms
