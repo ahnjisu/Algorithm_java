e59ed5fc55e66157ce0118ea2a99f6c25cc1802d
ahnjisu <an1302@hs.ac.kr>
2022-12-22 23:58:40 +0900

    Boj 18108 불기연도

---
+++ algorithm/src/algorithm/boj/Boj_18108.java
@@ -0,0 +1,19 @@
+package algorithm.boj;
+import java.util.*;
+// https://www.acmicpc.net/problem/18108
+
+public class Boj_18108 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int y = sc.nextInt();
+		
+		System.out.print(y-543);
+
+	}
+
+}
+
+// memory:17700KB, time:216ms
