7494dac3e719ac851b72e55a9637636fa077186a
ahnjisu <an1302@hs.ac.kr>
2022-12-23 23:59:09 +0900

    Boj 3003 체스말 문제

---
+++ algorithm/src/algorithm/boj/Boj_3003.java
@@ -0,0 +1,28 @@
+package algorithm.boj;
+import java.util.*;
+
+public class Boj_3003 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int k = 1;
+		int q = 1;
+		int r = 2;
+		int b = 2;
+		int n = 2;
+		int p = 8;
+		
+		k = k - sc.nextInt();
+		q = q - sc.nextInt();
+		r = r - sc.nextInt();
+		b = b - sc.nextInt();
+		n = n - sc.nextInt();
+		p = p - sc.nextInt();
+		
+		System.out.println(k+" "+q+" "+r+" "+b+" "+n+" "+p);
+		
+
+	}
+
+}
