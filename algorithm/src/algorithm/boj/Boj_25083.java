a9484694a7e7ef82ff40dc01d27fab6163778740
ahnjisu <an1302@hs.ac.kr>
2022-12-24 23:51:02 +0900

    Boj 25083 새싹 출력

---
+++ algorithm/src/algorithm/boj/Boj_25083.java
@@ -0,0 +1,13 @@
+package algorithm.boj;
+// https://www.acmicpc.net/problem/25083
+
+public class Boj_25083 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |");
+	}
+
+}
+
+// memory : 13888KB, time : 116ms
