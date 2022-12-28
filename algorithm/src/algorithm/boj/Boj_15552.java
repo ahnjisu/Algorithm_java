4029b82a310476c6cd445dba17c75210f35a767f
ahnjisu <an1302@hs.ac.kr>
2022-12-28 23:56:55 +0900

    Boj 15552 빠른 A+B

---
+++ algorithm/src/algorithm/boj/Boj_15552.java
@@ -0,0 +1,34 @@
+package algorithm.boj;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/15552
+
+public class Boj_15552 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		
+		for (int i = 0; i < T; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()))+ "\n");
+		}
+		br.close();
+        
+		bw.flush();
+		bw.close();
+ 
+	}
+
+}
+
+// memory:235992KB, time:888ms
